04863f25cac8631dedd4bafc6d1a24045a32f1d2
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2024-02-10 11:35:29 +0000

    auto-search-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
+
+
+let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
 const App = () => {
-  return (
+
+  let [search, setSearch] = useState("")
+
+  let [filterFruits, setFilterFruits] = useState(fruits)
+
+
+  useEffect(() => {
+
+    setFilterFruits( fruits.filter(value =>  value.toLowerCase().includes(search.toLowerCase()) ))
+        
+  },[search])
+
+
+
+  return(
     <div>
-        {/* Do not remove the main div */}
+             <input type="text" placeholder="Search"
+               onChange={e => setSearch(e.target.value)}
+             />
+             <ul>
+                  { 
+                     filterFruits.map(value =>(
+                        <li>{value}</li>
+                     ))
+                  }
+     
+             </ul>
+             
     </div>
   )
 }
 
+ 
 export default App
